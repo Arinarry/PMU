8c479da83c6205f1cbc85358d75e96e4cc9a7b09
Arinarry <arinarybakova2003@gmail.com>
2024-10-20 12:57:06 +0700

    Added a cube inscribed in the planet

--- ReferenceBook/app/src/main/java/com/example/referencebook/MyRender.kt
+++ ReferenceBook/app/src/main/java/com/example/referencebook/MyRender.kt
@@ -23,12 +23,11 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
     private val uranus = Sphere(context, 0.1f)
     private val neptune = Sphere(context, 0.1f)
     private val pluto = Sphere(context, 0.05f)
+    private val cube = Cube()
 
     private var sunRotationAngle = 0f
     private var earthAngle = 0f
-    private var earthRotationAngle = 0f
     private var moonAngle = 0f
-    private var moonRotationAngle = 0f
     private var mercuryAngle = 0f
     private var venusAngle = 0f
     private var marsAngle = 0f
@@ -41,13 +40,18 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
     private var width = 0
     private var height = 0
 
+    private var currentPlanetIndex = 0
+    private var planets = listOf(sun, mercury, venus, earth, moon, mars, jupiter, saturn, uranus, neptune, pluto)
+
     override fun onSurfaceCreated(gl: GL10, config: EGLConfig?) {
         gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
         gl.glEnable(GL10.GL_TEXTURE_2D)
         gl.glEnable(GL10.GL_DEPTH_TEST)
-        gl.glDepthFunc(GL10.GL_LEQUAL)
+        gl.glEnable(GL10.GL_BLEND)
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA)
+        /*gl.glDepthFunc(GL10.GL_LEQUAL)
         gl.glEnable(GL10.GL_CULL_FACE)
-        gl.glCullFace(GL10.GL_BACK)
+        gl.glCullFace(GL10.GL_BACK)*/
 
         texturedSquare.loadTexture(gl, R.drawable.galaxy)
         sun.loadTexture(gl, R.drawable.sun_texture, 0)
@@ -79,10 +83,11 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
     override fun onDrawFrame(gl: GL10) {
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT or GL10.GL_DEPTH_BUFFER_BIT)
         gl.glLoadIdentity()
+
         gl.glPushMatrix()
         gl.glTranslatef(0.0f, 0.0f, -6.0f)
         val aspectRatio = if (height != 0) width.toFloat() / height.toFloat() else 1f
-        gl.glScalef(aspectRatio * 3.0f, 3.0f, 1.0f)
+        gl.glScalef(aspectRatio * 3.0f,3.0f, 1.0f)
         gl.glBindTexture(GL10.GL_TEXTURE_2D, texturedSquare.getTextureId())
         texturedSquare.draw(gl)
         gl.glPopMatrix()
@@ -94,24 +99,31 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
         sun.draw(gl)
         gl.glPopMatrix()
 
-        drawPlanet(gl, mercury, mercuryAngle, 0.4f, 1.7f, 1) // Меркурий
-        drawPlanet(gl, venus, venusAngle, 0.6f, 1.5f, 2) // Венера
-        drawPlanet(gl, earth, earthAngle, 0.8f, 1.3f, 3) // Земля
-        drawMoon(gl, moon, earthAngle, moonAngle, 0.8f, 4) // Луна
-        drawPlanet(gl, mars, marsAngle, 1.0f, 1.1f, 5) // Марс
-        drawPlanet(gl, jupiter, jupiterAngle, 1.5f, 1.0f, 6) // Юпитер
-        drawPlanet(gl, saturn, saturnAngle, 1.7f, 0.7f, 7) // Сатурн
-        drawPlanet(gl, uranus, uranusAngle, 2.0f, 0.5f, 8) // Уран
-        drawPlanet(gl, neptune, neptuneAngle, 2.2f, 0.3f, 9) // Нептун
-        drawPlanet(gl, pluto, plutoAngle, 2.5f, 0.2f, 10) // Плутон
+        if (currentPlanetIndex == 0) {
+            gl.glPushMatrix()
+            gl.glTranslatef(0.0f, 0.0f, -5.0f)
+            val scaleCube = planets[currentPlanetIndex].radius()
+            gl.glScalef(scaleCube, scaleCube, scaleCube)
+            cube.draw(gl)
+            gl.glPopMatrix()
+        }
+
+        drawPlanet(gl, mercury, mercuryAngle, 0.4f, 1.7f, 1, 1) // Меркурий
+        drawPlanet(gl, venus, venusAngle, 0.6f, 1.5f, 2, 2) // Венера
+        drawPlanet(gl, earth, earthAngle, 0.8f, 1.3f, 3, 3) // Земля
+        drawMoon(gl, moon, earthAngle, moonAngle, 0.8f, 4, 4) // Луна
+        drawPlanet(gl, mars, marsAngle, 1.0f, 1.1f, 5, 5) // Марс
+        drawPlanet(gl, jupiter, jupiterAngle, 1.5f, 1.0f, 6, 6) // Юпитер
+        drawPlanet(gl, saturn, saturnAngle, 1.7f, 0.7f, 7, 7) // Сатурн
+        drawPlanet(gl, uranus, uranusAngle, 2.0f, 0.5f, 8, 8) // Уран
+        drawPlanet(gl, neptune, neptuneAngle, 2.2f, 0.3f, 9, 9) // Нептун
+        drawPlanet(gl, pluto, plutoAngle, 2.5f, 0.2f, 10, 10) // Плутон
 
         sunRotationAngle -= 1f
         mercuryAngle += 4f
         venusAngle += 3f
         earthAngle += 2f
-        earthRotationAngle += 1f
         moonAngle += 5f
-        moonRotationAngle += 3f
         marsAngle += 1.5f
         jupiterAngle += 1f
         saturnAngle += 0.7f
@@ -119,7 +131,7 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
         neptuneAngle += 0.3f
         plutoAngle += 0.2f
 
-        if (sunRotationAngle >= 360) sunRotationAngle = 0f
+        /*if (sunRotationAngle >= 360) sunRotationAngle = 0f
         if (mercuryAngle >= 360) mercuryAngle = 0f
         if (venusAngle >= 360) venusAngle = 0f
         if (earthAngle >= 360) earthAngle = 0f
@@ -129,37 +141,50 @@ class MyRender(context: Context) : GLSurfaceView.Renderer {
         if (saturnAngle >= 360) saturnAngle = 0f
         if (uranusAngle >= 360) uranusAngle = 0f
         if (neptuneAngle >= 360) neptuneAngle = 0f
-        if (plutoAngle >= 360) plutoAngle = 0f
+        if (plutoAngle >= 360) plutoAngle = 0f*/
     }
 
-    private fun drawPlanet(gl: GL10, planet: Sphere, angle: Float, radius: Float, rotationSpeed: Float, textureIndex: Int) {
+    private fun drawPlanet(gl: GL10, planet: Sphere, angle: Float, radius: Float, rotationSpeed: Float, textureIndex: Int, indexPlanet: Int) {
         gl.glPushMatrix()
-        gl.glTranslatef(
-            cos(Math.toRadians(angle.toDouble())).toFloat() * radius,
-            sin(Math.toRadians(angle.toDouble())).toFloat() * radius,
-            -5.2f
-        )
+        val x = cos(Math.toRadians(angle.toDouble())).toFloat() * radius
+        val y = sin(Math.toRadians(angle.toDouble())).toFloat() * radius
+        gl.glTranslatef(x,y,-5.2f)
         gl.glRotatef(-angle * rotationSpeed, 0.0f, 1.0f, 0.0f)
         gl.glBindTexture(GL10.GL_TEXTURE_2D, planet.getTextureId(textureIndex))
         planet.draw(gl)
         gl.glPopMatrix()
+
+        if (currentPlanetIndex == indexPlanet) {
+            gl.glPushMatrix()
+            gl.glTranslatef(x, y, -5.2f)
+            val scaleCube = planets[currentPlanetIndex].radius()
+            gl.glScalef(scaleCube, scaleCube, scaleCube)
+            cube.draw(gl)
+            gl.glPopMatrix()
+        }
     }
 
-    private fun drawMoon(gl: GL10, moon: Sphere, earthAngle: Float, moonAngle: Float, radius: Float, textureIndex: Int) {
+    private fun drawMoon(gl: GL10, moon: Sphere, earthAngle: Float, moonAngle: Float, radius: Float, textureIndex: Int, indexPlanet: Int) {
         gl.glPushMatrix()
 
         val earthX = cos(Math.toRadians(earthAngle.toDouble())).toFloat() * radius
         val earthY = sin(Math.toRadians(earthAngle.toDouble())).toFloat() * radius
 
-        gl.glTranslatef(earthX, earthY, -5.2f)
-
-        val moonDistance = 0.13f
+        gl.glTranslatef(earthX, earthY, -5.1f)
+        val moonDistance = 0.10f
         gl.glRotatef(moonAngle, 0.0f, 0.0f, 1.0f)
         gl.glTranslatef(moonDistance, 0.0f, 0.0f)
 
         gl.glBindTexture(GL10.GL_TEXTURE_2D, moon.getTextureId(textureIndex))
         moon.draw(gl)
 
+        if (currentPlanetIndex == indexPlanet) {
+            gl.glPushMatrix()
+            val scaleCube = moon.radius() * 1.1f
+            gl.glScalef(scaleCube, scaleCube, scaleCube)
+            cube.draw(gl)
+            gl.glPopMatrix()
+        }
         gl.glPopMatrix()
     }
 }
